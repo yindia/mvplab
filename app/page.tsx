92f29eec35817efa4c4936dd11f7e8adb6e62614
Yuvraj <evalsocket@gmail.com>
2025-07-21 14:32:21 +0900

    Remove unused VideoBackground import from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navigation from "@/components/Navigation";
 import HeroSection from "@/components/HeroSection";
 import WhyMVPSection from "@/components/WhyMVPSection";
 import ServicesSection from "@/components/ServicesSection";
-import VideoBackground from "@/components/VideoBackground";
 import PricingSection from "@/components/PricingSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import FAQSection from "@/components/FAQSection";
